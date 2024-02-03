c72c71a0452f76f7804fbfa1927d284e6dcf6c96
ibrahim_ahmad <ibrahimahmad.info@gmail.com>
2024-02-03 21:07:47 +0800

    Add equate function to perform calculation.

--- calculator.js
+++ calculator.js
@@ -5,19 +5,23 @@ let operation = '';
 
 // declare functions for basic computation
 function add(num1, num2){
-    return num1 + num2;
+    let result = Number(num1) + Number(num2);
+    return result.toString();
 };
 
 function subtract(num1, num2){
-    return num1 - num2;
+    let result = Number(num1) - Number(num2)
+    return result.toString();
 };
 
 function multiply(num1, num2){
-    return num1 * num2;
+    let result = Number(num1) * Number(num2);
+    return result.toString();
 };
 
 function divide(num1, num2){
-    return num1 / num2;
+    let result = Number(num1) / Number(num2);
+    return result.toString();
 }
 
 // function to perform operations
@@ -28,7 +32,7 @@ function operator(num1, num2, operation){
             return add(num1, num2);
             break;
 
-        case 'subtract':jh
+        case 'subtract':
             return subtract(num1, num2);
             break;
 
@@ -42,13 +46,10 @@ function operator(num1, num2, operation){
     };
 };
 
-// get info (number/operator/clear) on button click
+// get data (number/operator/clear) from button click
 getData = (event) => {
     let display = document.querySelector('.display_text');
     let className = event.target.className;
-
-    
-   
 
     if (event.target.tagName == 'BUTTON') {
         
@@ -59,19 +60,51 @@ getData = (event) => {
                 break;
             
             case 'clear':
+                prevNumText = '';                   // clear number variable
                 currNumText = '';                   // clear number variable
                 display.textContent = '0';          // reinitialized display to zero
 
                 // clear operator button styling
                 if (operation != ''){
-                    let buttonOperator = buttons.querySelector(`#${operation}`);
-                    buttonOperator.style.opacity = '';
+                    removeOperatorStyle();
+
+                    operation   = '';             // clear operator variable
                 };
                    
                 break;
             
             case 'operator':
+
+                // remove styling from previously selected operator before replace.
+                removeOperatorStyle();
+
+                // replace variable with new selected value
                 operation = event.target.id;
+
+                // once operator button is selected, save existing num as prevNum variable
+                // and then reinitialized the currNum.
+                if (currNumText != '') {
+                    prevNumText = currNumText;
+                    currNumText = '';
+                };
+
+                break;
+
+            case 'equate':
+                let checkVar = prevNumText != ''
+                                && currNumText != ''
+                                && operation != '';
+                
+                if (checkVar) {
+                    let result = operator(prevNumText, currNumText, operation);
+                    display.textContent = result;
+                    removeOperatorStyle();
+
+                    // save result as prevNum for later use & clear  currNum
+                    prevNumText = result;
+                    currNumText = '';
+                };
+
                 break;
 
         };
@@ -96,6 +129,13 @@ removeButtonStyle = (event) => {
             event.target.style.opacity = '';
     };
     
+};
+
+removeOperatorStyle = () => {
+    if (operation != '') {
+        let buttonOperator = buttons.querySelector(`#${operation}`);
+        buttonOperator.style.opacity = '';
+    };
 };
 
 
--- todo.md
+++ todo.md
@@ -1,3 +1,1 @@
-update 'operator' function to: 
-1. remove operator styling when another operator is being selected.
-2. save the current number as previousNumber varioable, and reinitilased currentNumber variable. Note: do this before updating the operation variable and only when operation variable was blank (to avoid issue when operator is reselected.) 
+
